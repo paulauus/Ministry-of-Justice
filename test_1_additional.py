7da2957c17dd08c1113eb49b5c8a050fe04ead01
Paula <trainee.paula.uusnakki@sigmalabs.co.uk>
2024-08-30 14:36:18 +0100

    More tests

--- test_1_additional.py
+++ test_1_additional.py
@@ -1,8 +1,8 @@
-from test_1 import is_log_line
+from test_1 import is_log_line, get_dict
 
 
 def test_valid_log_line():
-    # Valid log line example
+    """Tests valid log line is True."""
     line = "03/11/21 08:51:01 INFO    :.main: Using log level 511"
 
     # Call the function with the valid log line
@@ -13,10 +13,34 @@ def test_valid_log_line():
 
 
 def test_invalid_log_line():
+    """Tests invalid log line is False."""
     line = "INFO    :.main: Missing timestamp in this log line"
 
     # Call the function with the invalid log line
     result = is_log_line(line)
 
     # Assert that the result is False
-    assert result == False
+    assert result is False
+
+
+def test_get_dict():
+    """Tests function handles time, log and message."""
+    line = "04/11/21 10:55:21 WARNING :This is a test message"
+    expected = {
+        "timestamp": "04/11/21 10:55:21",
+        "log_level": "WARNING",
+        "message": ":This is a test message"
+    }
+    actual = get_dict(line)
+    assert expected == actual
+
+def test_get_dict_no_message():
+    """Tests function handles no message."""
+    line = "04/11/21 10:55:21 WARNING"
+    expected = {
+        "timestamp": "04/11/21 10:55:21",
+        "log_level": "WARNING",
+        "message": ""
+    }
+    actual = get_dict(line)
+    assert expected == actual
